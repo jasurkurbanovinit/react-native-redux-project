52928e897de57aad9570deced12cc49a37756d91
jasurkurbanovinit <jasurkurbanov96@gmail.com>
2020-11-25 11:39:07 +0500

    redux hooks added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,23 @@
 import React from 'react';
 import {Button, TextInput, Text, View} from 'react-native';
-import {connect} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {addArticle, setTitle} from './../redux/actions';
 
-const Form = ({title, setTitle, addArticle}) => {
+const Form = () => {
+  const title = useSelector((state) => state.title);
+  const dispatch = useDispatch();
+
   const handleInput = () => {
-    addArticle(title);
-    setTitle('');
+    dispatch(addArticle(title));
+    dispatch(setTitle(''));
   };
   return (
     <View>
       <Text>Enter text</Text>
-      <TextInput value={title} onChangeText={(e) => setTitle(e)} />
+      <TextInput value={title} onChangeText={(e) => dispatch(setTitle(e))} />
       <Button title="Add to store" onPress={handleInput} />
     </View>
   );
 };
 
-const mapStateToProps = (state) => ({
-  title: state.title,
-});
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    addArticle: (a) => dispatch(addArticle(a)),
-    setTitle: (a) => dispatch(setTitle(a)),
-  };
-};
-export default connect(mapStateToProps, mapDispatchToProps)(Form);
+export default Form;
